294b71dff21878cb066a596cf9c12e5a9a08a9bf
RAprogramm <andrey.rozanov.vl@gmail.com>
2025-11-17 08:20:11 +0700

    #33 fix: use Object::atomic instead of lambda

--- src/emu/tests_transitions.rs
+++ src/emu/tests_transitions.rs
@@ -84,7 +84,7 @@ fn test_propagate_value() {
 fn test_delegate_with_lambda() {
     use crate::atom;
     let mut emu = Emu::from_str("ν0 {}⤍ Φ̇").unwrap();
-    emu.objects[1] = crate::object::Object::lambda("test", atom::int_add);
+    emu.objects[1] = crate::object::Object::atomic("test".to_string(), atom::int_add);
     emu.inject(1, Basket::from_str("[ν1, ξ:β0, 𝜑→, α0⇶0x0001, α1⇶0x0002]").unwrap());
     let mut perf = Perf::new();
     emu.delegate(&mut perf, 1);
@@ -120,16 +120,12 @@ fn test_new_basket() {
 }
 
 #[test]
-fn test_stashed_finds_constant() {
-    let mut emu = Emu::from_str("ν0 {}⤍ Φ̇\nν1 {Δ ⤍ 0x002A}⤍ Φ̇").unwrap();
-    emu.inject(1, Basket::from_str("[ν1, ξ:β0, 𝜑⇶0x002A]").unwrap());
-    let result = emu.stashed(1, 0);
-    assert_eq!(result, Some(1));
-}
-
-#[test]
-fn test_stashed_not_found() {
-    let mut emu = Emu::from_str("ν0 {}⤍ Φ̇\nν1 {}").unwrap();
-    let result = emu.stashed(1, 0);
-    assert_eq!(result, None);
+fn test_new_finds_stashed_constant() {
+    let mut emu = Emu::from_str("ν0 {}⤍ Φ̇\nν1 {a ⤍ ν2}\nν2 {Δ ⤍ 0x002A}⤍ Φ̇").unwrap();
+    emu.inject(1, Basket::from_str("[ν1, ξ:β0]").unwrap());
+    emu.inject(2, Basket::from_str("[ν2, ξ:β0, 𝜑⇶0x002A]").unwrap());
+    emu.baskets[1].put(crate::loc::Loc::from_str("a").unwrap(), crate::basket::Kid::Need(2, 0));
+    let mut perf = Perf::new();
+    emu.new(&mut perf, 1, crate::loc::Loc::from_str("a").unwrap());
+    assert!(matches!(emu.basket(1).kids.get(&crate::loc::Loc::from_str("a").unwrap()), Some(crate::basket::Kid::Wait(2, _))));
 }
